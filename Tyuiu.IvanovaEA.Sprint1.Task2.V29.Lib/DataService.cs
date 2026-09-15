0171226926106a47ce8d11fa299e8541288b422d
“Екатерина <kataivanova0380@gmail.com>
2026-09-15 11:48:22 +0500

    Add Task2

---
+++ Tyuiu.IvanovaEA.Sprint1.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.IvanovaEA.Sprint1.Task2.V29.Lib
+{
+    public class DataService : ISprint1Task2V29
+    {
+        public int ConvertSecondsToHours(int value)
+        {
+            return value / 60;
+        }
+    }
+}
+    
---
+++ Tyuiu.IvanovaEA.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.IvanovaEA.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.IvanovaEA.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int x = 60;
+            //ConvertSecondsToHours(int)
+
+            var res = ds.ConvertSecondsToHours(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
